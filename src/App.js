3958caa27f3e0236ff9708b15f717cc78e231d88
Chingsan53 <cly53@csu.fullerton.edu>
2025-07-17 13:38:45 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Homescreen from "./components/Homescreen";
+
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-5xl font-extrabold text-blue-600 underline">
-        Tailwind is working!
-      </h1>
+      <Homescreen />
     </div>
   );
 }
---
+++ src/components/Homescreen.jsx
@@ -0,0 +1,9 @@
+const Homescreen = () => {
+  return (
+    <div>
+      <div className="text-5xl">This is homescreen</div>
+    </div>
+  );
+};
+
+export default Homescreen;
